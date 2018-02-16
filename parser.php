6b6ccd842446d5e3de9415fb9806061405403336
faridg <farid.gassanov@gmail.com>
2018-02-16 18:06:31 -0500

    just title and description for now

---
+++ parser.php
@@ -0,0 +1,22 @@
+<?php
+ function get_data($url) {
+    $browse = curl_init();
+    $timeout = 5;
+    curl_setopt($browse, CURLOPT_URL, $url);
+    curl_setopt($browse, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($browse, CURLOPT_CONNECTTIMEOUT, $timeout);
+    $data = curl_exec($browse);
+    curl_close($browse);
+    return $data;
+}
+
+$returned_content = get_data('https://newyork.craigslist.org/search/web?format=rss');
+
+$xml = simplexml_load_string($returned_content) or die("Wanderror");
+
+foreach($xml->item as $item) {
+    echo $item->title . "<br>";
+    echo $item->description . "<br>";
+    echo "<br>";
+}
+  ?>
